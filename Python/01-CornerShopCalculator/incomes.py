ee132be497e0e2efd523898c1afe88c1c3076fb4
MOHAMED Hamda <33846477+genidevelop@users.noreply.github.com>
2026-06-25 14:22:06 +0100

    Second stage: Measure total income

--- Python/01-CornerShopCalculator/incomes.py
+++ Python/01-CornerShopCalculator/incomes.py
@@ -1,7 +1,11 @@
-print("Prices:")
-print("Bubblegum: $2")
-print("Toffee: $0.2")
-print("Ice cream: $5")
-print("Milk chocolate: $4")
-print("Doughnut: $2.5")
-print("Pancake: $3.2")
+total_income = 202.0 + 118.0 + 2250.0 + 1680.0 + 1075.0 + 80.0
+
+print("Earned amount:")
+print("Bubblegum: $202")
+print("Toffee: $118")
+print("Ice cream: $2250")
+print("Milk chocolate: $1680")
+print("Doughnut: $1075")
+print("Pancake: $80")
+print()
+print("Income: $", total_income)
